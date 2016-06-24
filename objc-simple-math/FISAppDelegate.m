de64a9ff59d3d84f0640a2622022cef6df997e6e
OliviaGunton <olivia.gunton@gmail.com>
2016-06-24 16:49:31 -0400

    Did simple math exercises

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -7,11 +7,57 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here.
-     
-     */
+    NSInteger i = 0;
+    
+    //NSLog(@"%li", i);
+    
+    i = 1 + 1;
+    
+    NSLog(@"%li", i);
+    
+    i = (1000 / 3) % 24;
+    
+    NSLog(@"%li", i);
+    
+    NSInteger a = 0;
+    NSInteger b = 0;
+    
+    NSInteger c = a + b;
+    
+    NSLog(@"a: %li; b: %li; c: %li", a, b, c);
+    
+    a = 17;
+    
+    b = 29;
+    
+    NSLog(@"a: %li; b: %li; c: %li", a, b, c);
+    
+    c = a + b;
+    
+    NSLog(@"%li", c);
+    
+    NSUInteger u = 1;
+    
+   // NSLog(@"%lu", u);
+    
+   // u = -1;
+    
+    NSLog(@"%lu", u);
+    
+    BOOL threeIsEqualToThree = (3 == 3);
+    
+    NSLog(@"3 == 3: %d", threeIsEqualToThree);
+    
+    NSInteger x = (2 + 3) * 5;
+    
+    NSLog(@"%li", x);
+    
+    CGFloat f = 0;
+    
+    f = M_PI;
+    
+    NSLog(@"%.12f", f);
+    
     
     // do not alter
     return YES;  //
